791995acb18e0bf5d441188caa3b03fcbf789023
VG149 <92550652+VG149@users.noreply.github.com>
2024-11-08 21:03:55 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/commands/CopyCommand.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.commands;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+import refactoring_guru.command.example.editor.Editor;
+
+public class CopyCommand extends Command {
+
+    public CopyCommand(Editor editor) {
+        super(editor);
+    }
+
+    @Override
+    public boolean execute() {
+        editor.clipboard = editor.textField.getSelectedText();
+        return false;
+    }
+}
---
+++ src/main/java/com/mycompany/commands/PasteCommand.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.commands;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+import editor.*;
+
+public class PasteCommand extends Command {
+
+    public PasteCommand(Editor editor) {
+        super(editor);
+    }
+
+    @Override
+    public boolean execute() {
+        if (editor.clipboard == null || editor.clipboard.isEmpty()) return false;
+
+        backup();
+        editor.textField.insert(editor.clipboard, editor.textField.getCaretPosition());
+        return true;
+    }
+}
